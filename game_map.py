a63b4470a4972c2fd61c9500e892cea1342d4a04
Kyle Stewart <4b796c65@gmail.com>
2020-06-20 22:19:04 -0700

    Move tile array functions to another module.
    Better documents how the dtypes are setup.
    
    Adds a helper function to make it easier to make new tiles.

--- game_map.py
+++ game_map.py
@@ -1,22 +1,15 @@
 import numpy as np  # type: ignore
 from tcod.console import Console
 
-graphic_dt = np.dtype([("ch", np.int32), ("fg", "3B"), ("bg", "3B")])
-
-tile_dt = np.dtype(
-    [("walkable", np.bool), ("transparent", np.bool), ("dark", graphic_dt)]
-)
-
-floor = np.array((True, True, (ord(" "), 255, (50, 50, 150))), dtype=tile_dt)
-wall = np.array((False, False, (ord(" "), 255, (0, 0, 100))), dtype=tile_dt)
+import tile_types
 
 
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=floor, order="F")
+        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
 
-        self.tiles[30:33, 22] = wall
+        self.tiles[30:33, 22] = tile_types.wall
 
     def render(self, console: Console):
         console.tiles_rgb[0 : self.width, 0 : self.height] = self.tiles["dark"]
