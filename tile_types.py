5fbd1f6aef62039b300e287f0696dcb233ec8de8
Kyle Stewart <4b796c65@gmail.com>
2020-06-20 22:32:30 -0700

    Explain the keyword only parameters.

--- tile_types.py
+++ tile_types.py
@@ -22,7 +22,7 @@ tile_dt = np.dtype(
 
 
 def new_tile(
-    *,
+    *,  # Enforce the use of keywords, so that parameter order doesn't matter.
     walkable: int,
     transparent: int,
     dark: Tuple[int, Tuple[int, int, int], Tuple[int, int, int]],
