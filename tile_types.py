fadf42912650755886bc818835bd005b4f76f305
Kyle Stewart <4b796c65@gmail.com>
2020-06-20 22:29:34 -0700

    Minor documentation tweaks.

--- tile_types.py
+++ tile_types.py
@@ -11,12 +11,12 @@ graphic_dt = np.dtype(
     ]
 )
 
-# Tile data used for predefined tiles.
+# Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
         ("walkable", np.bool),  # True if this tile can be walked over.
         ("transparent", np.bool),  # True if this tile doesn't block FOV.
-        ("dark", graphic_dt),  # Graphics for when this tile is not visible.
+        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
     ]
 )
 
