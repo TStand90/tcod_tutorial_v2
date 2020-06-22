5ba623938f5ff1deb326f4f0f953ffe02cc3c372
Tyler Standridge <tstand90@gmail.com>
2020-06-22 07:21:33 -0700

    Part 3 finished

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ def main() -> None:
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
@@ -25,6 +29,14 @@ def main() -> None:
     entities = {npc, player}
 
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
