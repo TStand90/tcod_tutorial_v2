b3ddf3e356953428e63537588945f6c19c1719c4
Kyle Stewart <4b796c65@gmail.com>
2020-06-29 19:06:19 -0700

    Remove target parameter from MeleeAction.
    Also added a missing entity check to MovementAction.

--- actions.py
+++ actions.py
@@ -37,12 +37,11 @@ class ActionWithDirection(Action):
 
 
 class MeleeAction(ActionWithDirection):
-    def __init__(self, dx: int, dy: int, target: Entity):
-        super().__init__(dx, dy)
-
-        self.target = target
-
     def perform(self, engine: Engine, entity: Entity) -> None:
+        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+        if not target:
+            return  # No entity to attack.
+
         print(f"You kick the {self.target.name}, much to its annoyance!")
 
 
@@ -55,6 +54,8 @@ class MovementAction(ActionWithDirection):
             return  # Destination is out of bounds.
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return  # Destination is blocked by a tile.
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return  # Destination is blocked by an entity.
 
         entity.move(self.dx, self.dy)
 
@@ -64,10 +65,8 @@ class BumpAction(ActionWithDirection):
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
 
-        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
-
-        if target:
-            return MeleeAction(self.dx, self.dy, target).perform(engine, entity)
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine, entity)
 
         else:
             return MovementAction(self.dx, self.dy).perform(engine, entity)
