3ea4c8f7d8f895c1451e7e213b5671eb4713ce65
Tyler Standridge <tstand90@gmail.com>
2020-07-02 18:26:45 -0700

    Sorting entities for rendering based on RenderOrder

--- game_map.py
+++ game_map.py
@@ -50,6 +50,8 @@ class GameMap:
             default=tile_types.SHROUD
         )
 
-        for entity in self.entities:
+        entities_sorted_for_rendering = sorted(self.entities, key=lambda x: x.render_order.value)
+
+        for entity in entities_sorted_for_rendering:
             if self.visible[entity.x, entity.y]:
                 console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
