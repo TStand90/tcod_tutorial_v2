3b89da408384902ce93fdeb44125397f23572407
Kyle Stewart <4b796c65@gmail.com>
2020-07-06 14:21:16 -0700

    Don't import Actor in the actions module.
    This was for an instance check which was moved to the game_map module.

--- actions.py
+++ actions.py
@@ -2,10 +2,9 @@ from __future__ import annotations
 
 from typing import Optional, Tuple, TYPE_CHECKING
 
-from entity import Actor
-
 if TYPE_CHECKING:
     from engine import Engine
+    from entity import Actor
     from entity import Entity
 
 
