4460fd5031c3a6dc30af6f8a87c8bb0942e95f47
Kyle Stewart <4b796c65@gmail.com>
2020-07-06 17:56:46 -0700

    Update to a smarter pathfinder.  Enemies will now move around each other.
    This may require an update to tcod, as this uses a newer pathfinder API.

--- components/ai.py
+++ components/ai.py
@@ -1,35 +1,72 @@
 from __future__ import annotations
 
-import math
+from typing import List, Tuple, TYPE_CHECKING
+
+import numpy as np  # type: ignore
+import tcod
 
 from actions import Action, MeleeAction, MovementAction, WaitAction
 from components.base_component import BaseComponent
+
+if TYPE_CHECKING:
+    from entity import Actor
 
 
 class BaseAI(Action, BaseComponent):
     def perform(self) -> None:
         raise NotImplementedError()
 
+    def get_path_to(self, dest_x: int, dest_y: int) -> List[Tuple[int, int]]:
+        """Compute and return a path to the target position.
+
+        If there is no valid path then returns an empty list.
+        """
+        # Copy the walkable array.
+        cost = np.array(self.entity.gamemap.tiles["walkable"], dtype=np.int8)
+
+        for entity in self.entity.gamemap.entities:
+            # Check that an enitiy blocks movement and the cost isn't zero (blocking.)
+            if entity.blocks_movement and cost[entity.x, entity.y]:
+                # Add to the cost of a blocked position.
+                # A lower number means more enemies will crowd behind each other in
+                # hallways.  A higher number means enemies will take longer paths in
+                # order to surround the player.
+                cost[entity.x, entity.y] += 10
+
+        # Create a graph from the cost array and pass that graph to a new pathfinder.
+        graph = tcod.path.SimpleGraph(cost=cost, cardinal=2, diagonal=3)
+        pathfinder = tcod.path.Pathfinder(graph)
+
+        pathfinder.add_root((self.entity.x, self.entity.y))  # Start position.
+
+        # Compute the path to the destination and remove the starting point.
+        path: List[List[int]] = pathfinder.path_to((dest_x, dest_y))[1:].tolist()
+
+        # Convert from List[List[int]] to List[Tuple[int, int]].
+        return [(index[0], index[1]) for index in path]
+
 
 class HostileEnemy(BaseAI):
+    def __init__(self, entity: Actor):
+        super().__init__(entity)
+        self.path: List[Tuple[int, int]] = []
+
     def perform(self) -> None:
         target = self.engine.player
         dx = target.x - self.entity.x
         dy = target.y - self.entity.y
-        distance = math.sqrt(dx ** 2 + dy ** 2)
-
-        dx = int(round(dx / distance))
-        dy = int(round(dy / distance))
-
-        action: Action = WaitAction(self.entity)
+        distance = max(abs(dx), abs(dy))  # Chebyshev distance.
 
         if self.engine.game_map.visible[self.entity.x, self.entity.y]:
-            if distance >= 2:
-                dest_x, dest_y = self.entity.get_path_astar(target.x, target.y)[0]
-                action = MovementAction(
-                    self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
-                )
-            else:
-                action = MeleeAction(self.entity, dx, dy)
+            if distance <= 1:
+                return MeleeAction(self.entity, dx, dy).perform()
 
-        return action.perform()
+            self.path = self.get_path_to(target.x, target.y)
+
+        if self.path:
+            dest_x, dest_y = self.path.pop(0)
+            return MovementAction(
+                self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
+            ).perform()
+
+        return WaitAction(self.entity).perform()
