000e32d39eb1249a179f54f53a942242b5eae6e8
Tyler Standridge <tstand90@gmail.com>
2020-07-14 09:33:56 -0700

    Adding a 'health_recovered' color, and using it in consumable.py

--- components/consumable.py
+++ components/consumable.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import color
 from components.base_component import BaseComponent
 from exceptions import Impossible
 
@@ -26,7 +27,7 @@ class HealingConsumable(Consumable):
         if amount_recovered > 0:
             self.engine.message_log.add_message(
                 f"You consume the {self.parent.name}, and recover {amount_recovered} HP!",
-                (0, 255, 0),
+                color.health_recovered,
             )
         else:
             raise Impossible(f"Your health is already full.")
