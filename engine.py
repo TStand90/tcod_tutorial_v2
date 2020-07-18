ae84a5c4f4cabf2c56fb7767b03e996ae4fd8451
Kyle Stewart <4b796c65@gmail.com>
2020-07-17 23:15:50 -0700

    Open save file for writing only after the data is serialized.
    This will prevent an error while saving from deleting a valid save.

--- engine.py
+++ engine.py
@@ -61,5 +61,6 @@ class Engine:
 
     def save_as(self, filename: str) -> None:
         """Save this Engine instance as a compressed file."""
+        save_data = lzma.compress(pickle.dumps(self))
         with open(filename, "wb") as f:
-            f.write(lzma.compress(pickle.dumps(self)))
+            f.write(save_data)
