471bd563e8c2c32bc4500d70f4d130f6a1f24324
Tyler Standridge <tstand90@gmail.com>
2020-09-18 15:19:28 -0700

    Removing max_monsters_per_room and max_items_per_room variables, no longer needed

--- game_map.py
+++ game_map.py
@@ -111,8 +111,6 @@ class GameWorld:
         max_rooms: int,
         room_min_size: int,
         room_max_size: int,
-        max_monsters_per_room: int,
-        max_items_per_room: int,
         current_floor: int = 0
     ):
         self.engine = engine
@@ -124,9 +122,6 @@ class GameWorld:
 
         self.room_min_size = room_min_size
         self.room_max_size = room_max_size
-
-        self.max_monsters_per_room = max_monsters_per_room
-        self.max_items_per_room = max_items_per_room
 
         self.current_floor = current_floor
 
--- setup_game.py
+++ setup_game.py
@@ -29,9 +29,6 @@ def new_game() -> Engine:
     room_min_size = 6
     max_rooms = 30
 
-    max_monsters_per_room = 2
-    max_items_per_room = 2
-
     player = copy.deepcopy(entity_factories.player)
 
     engine = Engine(player=player)
@@ -43,8 +40,6 @@ def new_game() -> Engine:
         room_max_size=room_max_size,
         map_width=map_width,
         map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
     )
 
     engine.game_world.generate_floor()
