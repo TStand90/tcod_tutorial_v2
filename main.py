5523a5fe1410a388816c13136f94c29a26fbe1e5
Kyle Benesch <4b796c65+github@gmail.com>
2021-06-17 23:04:13 -0700

    Setup PyInstaller deployment.
    Move font to a specific data directory and set it to be packaged.
    
    Add an icon and spec file.
    
    Add an automated packaging workflow.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main() -> None:
     player_x = int(screen_width / 2)
     player_y = int(screen_height / 2)
 
-    tileset = tcod.tileset.load_tilesheet("dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
+    tileset = tcod.tileset.load_tilesheet("data/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
 
     event_handler = EventHandler()
 
