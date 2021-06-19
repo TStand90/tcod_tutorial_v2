a39a61ca3110fb700f06566d114665ca42c58721
Kyle Benesch <4b796c65+github@gmail.com>
2021-06-18 20:51:10 -0700

    Replace unused name with `_`.

--- procgen.py
+++ procgen.py
@@ -67,7 +67,7 @@ def generate_dungeon(
 
     rooms: List[RectangularRoom] = []
 
-    for r in range(max_rooms):
+    for _ in range(max_rooms):
         room_width = random.randint(room_min_size, room_max_size)
         room_height = random.randint(room_min_size, room_max_size)
 
