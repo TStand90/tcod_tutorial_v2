5f08e8e839565d654b8c7862a12ec1cb00f37cbc
Kyle Benesch <4b796c65+github@gmail.com>
2021-07-02 12:33:05 -0700

    Simplify part-1 to a single script.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 import tcod
 
-import game.actions
-import game.input_handlers
+MOVE_KEYS = {
+    tcod.event.K_UP: (0, -1),
+    tcod.event.K_DOWN: (0, 1),
+    tcod.event.K_LEFT: (-1, 0),
+    tcod.event.K_RIGHT: (1, 0),
+}
 
 
 def main() -> None:
@@ -13,8 +17,6 @@ def main() -> None:
     player_y: int = screen_height // 2
 
     tileset = tcod.tileset.load_tilesheet("data/dejavu16x16_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD)
-
-    event_handler = game.input_handlers.EventHandler()
 
     with tcod.context.new(
         columns=screen_width,
@@ -32,13 +34,15 @@ def main() -> None:
             root_console.clear()
 
             for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if isinstance(action, game.actions.Move):
-                    new_x = player_x + action.dx
-                    new_y = player_y + action.dy
-                    if 0 <= new_x < screen_width and 0 <= new_y < screen_height:
-                        player_x, player_y = new_x, new_y
+                if isinstance(event, tcod.event.Quit):
+                    raise SystemExit(0)
+                if isinstance(event, tcod.event.KeyDown):
+                    if event.sym in MOVE_KEYS:
+                        delta_x, delta_y = MOVE_KEYS[event.sym]
+                        dest_x = player_x + delta_x
+                        dest_y = player_y + delta_y
+                        if 0 <= dest_x < screen_width and 0 <= dest_y < screen_height:
+                            player_x, player_y = dest_x, dest_y
 
 
 if __name__ == "__main__":
