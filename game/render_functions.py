3fc955088fc137a3d7b2be5e86c0ed691a430ef3
Kyle Benesch <4b796c65+github@gmail.com>
2021-07-22 16:16:46 -0700

    Fix hard-coded render bar width.
    Related to #26

--- game/render_functions.py
+++ game/render_functions.py
@@ -19,7 +19,7 @@ def get_names_at_location(x: int, y: int, game_map: game.game_map.GameMap) -> st
 def render_bar(console: tcod.Console, current_value: int, maximum_value: int, total_width: int) -> None:
     bar_width = int(float(current_value) / maximum_value * total_width)
 
-    console.draw_rect(x=0, y=45, width=20, height=1, ch=1, bg=game.color.bar_empty)
+    console.draw_rect(x=0, y=45, width=total_width, height=1, ch=1, bg=game.color.bar_empty)
 
     if bar_width > 0:
         console.draw_rect(x=0, y=45, width=bar_width, height=1, ch=1, bg=game.color.bar_filled)
